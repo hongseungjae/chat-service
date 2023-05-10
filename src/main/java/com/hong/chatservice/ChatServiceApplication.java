0d04238afb2835a37f07490efbb68df172ac2d54
hongseungjae <ghdtmdwo@gmail.com>
2023-05-10 23:16:08 +0900

    feat: BaseEntity 활성화를 위해 @EnableJpaAuditing 설정

--- src/main/java/com/hong/chatservice/ChatServiceApplication.java
+++ src/main/java/com/hong/chatservice/ChatServiceApplication.java
@@ -2,8 +2,10 @@ package com.hong.chatservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ChatServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/hong/chatservice/setting/BaseEntity.java
+++ src/main/java/com/hong/chatservice/setting/BaseEntity.java
@@ -8,15 +8,16 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 @MappedSuperclass
-public class BaseEntity {
+public abstract class BaseEntity implements Serializable {
 
     @CreatedDate
-    @Column(updatable = false)
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
