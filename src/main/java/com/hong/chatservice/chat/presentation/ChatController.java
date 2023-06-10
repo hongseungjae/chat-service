a6dc75ccc625c6d684e0febdf48d3a9333c2f356
hongseungjae <ghdtmdwo@gmail.com>
2023-06-11 01:42:00 +0900

    feat: 헤더에서 멤버이름 추출 후 ChatMessage에 세팅 후 반환

--- src/main/java/com/hong/chatservice/chat/presentation/ChatController.java
+++ src/main/java/com/hong/chatservice/chat/presentation/ChatController.java
@@ -1,16 +1,15 @@
 package com.hong.chatservice.chat.presentation;
 
 import com.hong.chatservice.chat.application.ChatService;
+import com.hong.chatservice.member.application.jwt.JwtUtil;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
-
-import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,13 +17,16 @@ import java.util.Map;
 public class ChatController {
 
     private final SimpMessagingTemplate template;
+    private final JwtUtil jwtUtil;
     private final ChatService chatService;
 
-
-
     @MessageMapping("/chat")
-    public void createPost(@Headers Map<String, Object> headers,
+    public void createPost(StompHeaderAccessor accessor,
                            @RequestBody @Valid ChatMessage chatMessage) {
+
+        String token = accessor.getFirstNativeHeader(JwtUtil.HEADER_STRING);
+        String username = jwtUtil.validateToken(token);
+        chatMessage.setMemberName(username);
 
         template.convertAndSend("/topic/chat/" + chatMessage.getRoomId(), chatMessage);
         //chatService.createChat(chatMessage);
