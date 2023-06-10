10df857b24782643257271f7d38e16acc2c19bc8
hongseungjae <ghdtmdwo@gmail.com>
2023-06-11 01:42:42 +0900

    feat: SubscribeEvent 발생 시 헤더에서 유저네임 추출 후 세팅

--- src/main/java/com/hong/chatservice/chat/application/EnterRoomEvent.java
+++ src/main/java/com/hong/chatservice/chat/application/EnterRoomEvent.java
@@ -1,29 +1,34 @@
 package com.hong.chatservice.chat.application;
 
 import com.hong.chatservice.chat.presentation.ServerMessage;
+import com.hong.chatservice.member.application.jwt.JwtUtil;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.socket.messaging.SessionSubscribeEvent;
 
-
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class EnterRoomEvent {
 
-    @Autowired
-    private SimpMessagingTemplate template;
+    private final SimpMessagingTemplate template;
+    private final JwtUtil jwtUtil;
+    static private final String SERVER_NAME = "server";
+
 
     @EventListener
     public void handleSubscribeEvent(SessionSubscribeEvent event) {
-        StompHeaderAccessor sha = StompHeaderAccessor.wrap(event.getMessage());
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
 
-        String destination = sha.getFirstNativeHeader(StompHeaderAccessor.STOMP_DESTINATION_HEADER);
+        String token = accessor.getFirstNativeHeader(JwtUtil.HEADER_STRING);
+        String username = jwtUtil.validateToken(token);
 
-        ServerMessage serverMessage = new ServerMessage("server", "승재님이 입장하였습니다.");
+        String destination = accessor.getFirstNativeHeader(StompHeaderAccessor.STOMP_DESTINATION_HEADER);
+        ServerMessage serverMessage = new ServerMessage(SERVER_NAME, String.format("%s님이 입장하였습니다.", username));
 
         template.convertAndSend(destination, serverMessage);
     }
