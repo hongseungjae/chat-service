2127e903c6461214f60af00c8659716d3eb0b7eb
hongseungjae <ghdtmdwo@gmail.com>
2023-06-11 01:43:53 +0900

    feat: 웹소켓 CONNECT 시 인증 검사

---
+++ src/main/java/com/hong/chatservice/chat/config/JwtChannelInterceptor.java
@@ -0,0 +1,38 @@
+package com.hong.chatservice.chat.config;
+
+import com.hong.chatservice.member.application.jwt.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+
+import java.util.List;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Slf4j
+public class JwtChannelInterceptor implements ChannelInterceptor {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+
+        log.info("command = {}",accessor.getCommand());
+        log.info("token = {}",accessor.getNativeHeader(JwtUtil.HEADER_STRING));
+
+        if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+            String token = accessor.getFirstNativeHeader(JwtUtil.HEADER_STRING);
+            jwtUtil.validateToken(token);
+        }
+
+        return message;
+    }
+}
