f78cbdc01337dcee76812e76b02943c9b559ca7e
hongseungjae <ghdtmdwo123@naver.com>
2023-06-18 21:48:10 +0900

    fix: Authentication Exception시 Json형태의 에러 반환

--- src/main/java/com/hong/chatservice/member/application/jwt/CustomAuthenticationFailureHandler.java
+++ src/main/java/com/hong/chatservice/member/application/jwt/CustomAuthenticationFailureHandler.java
@@ -1,32 +1,39 @@
 package com.hong.chatservice.member.application.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hong.chatservice.setting.exception.CustomErrorResponse;
+import com.hong.chatservice.setting.exception.ErrorCode;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Component
+@Slf4j
 public class CustomAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
 
-        String errorMessage = "Invalid Username or Password"; // 기본 예외 메시지
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType(MediaType.APPLICATION_JSON.getType());
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
 
-        if(exception instanceof BadCredentialsException) {
-            errorMessage = "Invalid Username or Password";
-        }else if(exception instanceof InsufficientAuthenticationException) {
-            errorMessage = "Invalid Secret Key";
+        try {
+            CustomErrorResponse customErrorResponse = new CustomErrorResponse(ErrorCode.INVALID_USER);
+            String json = new ObjectMapper().writeValueAsString(customErrorResponse);
+            response.getWriter().write(json);
+        } catch (Exception e) {
+            log.error(e.getMessage());
         }
 
-        response.sendError(HttpStatus.UNAUTHORIZED.value(), errorMessage);
-        //super.onAuthenticationFailure(request, response, exception);
     }
 }
